f9ca0b36b8cdb4e010bb5515a91a9b3f9388bb75
Saumya1213 <148684318+Saumya1213@users.noreply.github.com>
2024-06-04 17:18:04 +0530

    Time: 11 ms (45.14%) | Memory: 11 MB (33.43%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,24 +1,50 @@
 class Solution {
 private:
-    int f(int ind, int n, vector<int>&arr, int k, vector<int>&dp){
-        if(ind==n) return 0;
+    // Memoization function
+    
+    int f(int ind, int k, vector<int>&arr, int n, vector<int>&dp){
+        if(ind>=n) return 0;
         if(dp[ind]!=-1) return dp[ind];
-        int len=0;
-        int maxi=INT_MIN;
-        int maxSum=0;
 
-        for(int i=ind;i<min(n,ind+k);i++){
+        int res = 0; 
+        int curMax=0;
+        int len=0;
+        for(int i=ind;i<min(ind+k,n);i++){
+            
+            curMax = max(arr[i],curMax);
             len++;
-            maxi=max(maxi,arr[i]);
-            int sum=len*maxi+f(i+1,n,arr,k,dp);
-            maxSum=max(sum,maxSum);
+            int sum = len*curMax + f(i+1,k,arr,n,dp);
+            res = max(sum,res);
+            
         }
-        return dp[ind]=maxSum;
+        return dp[ind] = res;
     }
+
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n=arr.size();
-        vector<int>dp(n,-1);
-        return f(0,n,arr,k,dp);
+        //vector<int>dp(n,-1); Memoization dp vector
+        vector<int>dp(n+1,0);
+
+        // Tabulation approach
+        for(int ind=n-1;ind>=0;ind--){
+            int res = 0; 
+            int curMax=0;
+            int len=0;
+
+            for(int i=ind;i<min(ind+k,n);i++){
+            
+            curMax = max(arr[i],curMax);
+            len++;
+            int sum = len*curMax + dp[i+1];
+            res = max(sum,res);
+            
+        }
+        dp[ind] = res;
+        }
+
+        return dp[0];
+
+        // return f(0,k,arr,dp); memoization function call 
     }
 };
