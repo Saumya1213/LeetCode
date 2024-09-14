30616e23ab879bcc781a68eccc649eeb7c621186
Saumya1213 <148684318+Saumya1213@users.noreply.github.com>
2024-09-15 00:21:40 +0530

    Time: 596 ms (38.65%) | Memory: 251.8 MB (98.15%) - LeetSync

--- 3495-k-th-nearest-obstacle-queries/k-th-nearest-obstacle-queries.cpp
+++ 3495-k-th-nearest-obstacle-queries/k-th-nearest-obstacle-queries.cpp
@@ -2,12 +2,17 @@ class Solution {
 public:
     vector<int> resultsArray(vector<vector<int>>& queries, int k) {
         int n=queries.size();
-        priority_queue<int>dist;
-        vector<int>res(n);
-        for(int i=0;i<n;i++){
+        vector<int>res(n,-1);
+        if(n<k){
+            return res;
+        } 
+        else{
+            priority_queue<int>dist;
+            for(int i=0;i<n;i++){
             dist.push(abs(queries[i][0])+abs(queries[i][1]));
             if(dist.size()>k) dist.pop();
             res[i] = (dist.size()==k)?dist.top():-1;
+        }
         }
         return res;
     }
